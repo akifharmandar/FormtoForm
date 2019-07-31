d7b87ebc744ffe9c850bf7adae6b6937829db29c
akifharmandar <akifharmandar@hotmail.com>
2019-08-01 00:39:11 +0300

    homework

---
+++ OOP.Form1_to_Form2_Add/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP.Form1_to_Form2_Add
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static Kisi[] person = new Kisi[] { };
+        public static int index = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kisi kk = new Kisi(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text);
+            index++;
+            Array.Resize(ref person, person.Length + 1);
+            person[index - 1] = kk;
+
+        }
+        Form2 frm2 = new Form2();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            index = 0;
+            frm2.Show();
+
+            this.Hide();
+            Array.Clear(person, 0, person.Length);
+        }
+    }
+}
---
+++ OOP.Form1_to_Form2_Add/Kisi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Form1_to_Form2_Add
+{
+    class Kisi
+    {
+        public Kisi(string ad,string soyad,string tel)
+        {
+            this.AD = ad;
+            this.SOYAD = soyad;
+            this.TEL = tel;
+        }
+        public Kisi(string ad,string soyad, string tel,string mail):this(ad,soyad,tel)
+        {
+            this.MAIL = mail;
+        }
+
+
+
+        public string AD{ get; set; }
+        public string SOYAD { get; set; }
+        public string TEL { get; set; }
+        public String MAIL { get; set; }
+
+
+        public override string ToString()
+        {
+            return this.AD +" "+this.SOYAD+" "+this.TEL+" ";
+        }
+    }
+}
